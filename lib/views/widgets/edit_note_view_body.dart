668ab537ae84ffc723bc3313729215593172d877
MariamEssam <romaessam570@gmail>
2024-12-02 00:53:12 +0200

    edit note view ui done

--- lib/views/widgets/edit_note_view_body.dart
+++ lib/views/widgets/edit_note_view_body.dart
@@ -1,23 +1,38 @@
 import 'package:flutter/material.dart';
 import 'package:note/views/widgets/custom_appbar.dart';
+import 'package:note/views/widgets/custom_text_field.dart';
 
 class EditNoteViewBody extends StatelessWidget {
   const EditNoteViewBody({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Padding(
-      padding: const EdgeInsets.symmetric(horizontal: 24),
-      child: Column(
-        children: const [
-          SizedBox(
-            height: 50,
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 24),
+        child: SingleChildScrollView(
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            children: const [
+              SizedBox(
+                height: 50,
+              ),
+              CutomAppBar(
+                title: "Edit Note",
+                icon: Icons.check,
+              ),
+              SizedBox(
+                height: 50,
+              ),
+              CustomTextField(hint: "Title"),
+              SizedBox(height: 16),
+              CustomTextField(
+                hint: "Content",
+                maxLines: 5,
+              ),
+            ],
           ),
-          CutomAppBar(
-            title: "Edit Note",
-            icon: Icons.check,
-          )
-        ],
+        ),
       ),
     );
   }
